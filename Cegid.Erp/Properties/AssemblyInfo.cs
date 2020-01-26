c0fdf1177c6c990d0e38d50048c245cf80232ec5
Fabrice Paillard <fpaillard@cegid.com>
2020-01-26 01:10:08 +0100

    test

--- Cegid.Erp/Properties/AssemblyInfo.cs
+++ Cegid.Erp/Properties/AssemblyInfo.cs
@@ -18,7 +18,4 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("bb154e41-e6ba-48da-bf16-113ef7bbbc37")]
 
-[assembly: InternalsVisibleTo("Cegid.Erp.Tests")]
-[assembly: InternalsVisibleTo("Cegid.Erp.Esker")]
-[assembly: InternalsVisibleTo("Cegid.Erp.Esker.Tests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
